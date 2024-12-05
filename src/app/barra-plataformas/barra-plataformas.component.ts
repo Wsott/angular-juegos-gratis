7e61f3153899f559445c899a43dbe1a198576569
Emiliano Gomez Olivera <znasters401@gmail.com>
2024-12-05 18:01:55 -0300

    Agregado estilos de la barra de navegacion

--- src/app/barra-plataformas/barra-plataformas.component.ts
+++ src/app/barra-plataformas/barra-plataformas.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BotonNavBarComponent } from '../boton-nav-bar/boton-nav-bar.component';
 
 @Component({
   selector: 'platform-navbar',
-  imports: [],
+  imports: [BotonNavBarComponent],
   templateUrl: './barra-plataformas.component.html',
   styleUrl: './barra-plataformas.component.css'
 })
