28593a75a45177a26487e673d5fb83eea20fc803
Emiliano Gomez Olivera <znasters401@gmail.com>
2024-12-08 01:00:26 -0300

    Estilos para mobile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,8 +12,10 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
   private _titulo: string = "";
+  mobile: boolean = false;
   serviceJuegos: JuegoService = inject(JuegoService);
-  listaDeJuegos: Juego[][] = []
+  listaDeJuegos: Juego[][] = [];
+  listaOriginal: Juego[] = [];
 
   @Input() set titulo(valor: string) {
     this._titulo = valor;
@@ -22,6 +24,16 @@ export class HomeComponent {
 
   get titulo(): string {
     return this._titulo;
+  }
+
+  ngOnInit(): void {
+    this.verificarPantalla();
+  }
+
+  verificarPantalla() {
+    if (window.innerWidth < 800) {
+      this.mobile = true;
+    }
   }
 
   async actualizarLista() {
@@ -35,7 +47,19 @@ export class HomeComponent {
 
       const datos = await this.serviceJuegos.obtenerListaPlataforma(parametro);
 
+      this.listaOriginal = datos;
+
       const sublistas = [];
+
+      if (this.mobile) {
+        for (let x = 0; x < datos.length; x += 1) {
+          sublistas.push(datos.slice(x, x + 1));
+        }
+
+        this.listaDeJuegos = sublistas;
+        return
+      }
+
       for (let x = 0; x < datos.length; x += 4) {
         sublistas.push(datos.slice(x, x + 4));
       }
