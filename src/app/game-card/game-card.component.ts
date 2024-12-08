96a7ff58b4cb0be311f6440541e12c3a77b9cb2d
Emiliano Gomez Olivera <znasters401@gmail.com>
2024-12-08 14:28:13 -0300

    Agregado spinner de carga para las imagenes

--- src/app/game-card/game-card.component.ts
+++ src/app/game-card/game-card.component.ts
@@ -1,13 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Juego } from '../juego';
 import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-game-card',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, SpinnerComponent ],
   templateUrl: './game-card.component.html',
   styleUrl: './game-card.component.css'
 })
 export class GameCardComponent {
   @Input() datos?: Juego;
+  cargando: boolean = true;
+
+  imagenCargada(): void {
+    this.cargando = false;
+  }
 }
